8ce8a86282d6d87c0f6279fe94e3f75fc9c8aeec
Tempan <johannestemnell@hotmail.com>
2015-02-25 10:38:46 +0100

    bla bla

--- Planetlab/Planetlab_Client/client.c
+++ Planetlab/Planetlab_Client/client.c
@@ -22,7 +22,7 @@ void main(void)
 	struct pt newplanet;
 
 	int a = 1;
-	int b = 2;
+	int b = 4;
 	char message[100000];
 	HANDLE mailSlot;
 	DWORD bytesWritten;
@@ -38,25 +38,29 @@ void main(void)
 
 	/* NOTE: replace code below for sending planet data to the server. */
 
-	while(1) 
+	strcpy_s(newplanet.name, sizeof(newplanet.name), "Första planeten");
+	newplanet.sx = 100;											
+	newplanet.sy = 100;											
+	newplanet.vx = 1;											
+	newplanet.vy = 1;											
+	newplanet.mass = 50;											
+	newplanet.life = 50;
+	newplanet.next = NULL;
+	while(a<b) 
 	{
-		/* send a friendly greeting to the server */
-		/* NOTE: The messages sent to the server need not to be of equal size.       */
-		/* Messages can be of different sizes as long as they don't exceed the       */
-		/* maximum message size that the mailslot can handle (defined upon creation).*/
-		
-			
-			strcpy_s(newplanet.name, sizeof(newplanet.name), "Första planeten");
-			newplanet.sx = fgets(message, sizeof (message), stdin);											
-			newplanet.sy = 100;											
-			newplanet.vx = 1;											
-			newplanet.vy = 1;											
-			newplanet.mass = 50;											
-			newplanet.life = 50;
-			newplanet.next = NULL;
+		/* send a friendly greeting to the server 
+		/NOTE: The messages sent to the server need not to be of equal size.
+		Messages can be of different sizes as long as they don't exceed the  
+		maximum message size that the mailslot can handle (defined upon creation).*/
 
-
-		//struct pt planet = {"Planet1",0,0,0,0,0,0,0,0}; 
+		strcpy_s(newplanet.name, sizeof(newplanet.name), "Första planeten");
+		newplanet.sx += 100;											
+		newplanet.sy += 100;											
+		newplanet.vx += 1;											
+		newplanet.vy += 1;											
+		newplanet.mass += 50;											
+		newplanet.life += 50;
+		newplanet.next = NULL;
 		/*(struct pt*)malloc(sizeof(struct pt));  // Malloc = Allocates a block of size bytes of memory
 		strcpy_s(planet->name, sizeof(planet->name), "Första planeten");*/
 
@@ -66,7 +70,6 @@ void main(void)
 			printf("data sent to server (bytes = %d), (name = %s) \n", bytesWritten, newplanet.name);
 		else
 			printf("failed sending data to server\n");
-
 		a++;
 	}
 
